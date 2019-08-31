8655030ad750a42d7a4f8808a71eeb8b3fd79139
James Riall <jamesrariall@gmail.com>
2019-08-31 17:45:14 +0100

    Add config from dotenv to flask app

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from config import Config
 
+Config.init()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+
+if __name__ == '__main__':
+  app.run(debug=Config.DEBUG, host=Config.HOST, port=Config.PORT)
---
+++ backend/config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+
+class Config:
+  """Contains the configuration for the application."""
+
+  @classmethod
+  def init(cls):
+    load_dotenv()
+
+    cls.HOST = os.getenv('HOST', 'localhost')
+    cls.PORT = os.getenv('PORT', '3000')
+    cls.DEBUG = os.getenv('DEBUG', True)
