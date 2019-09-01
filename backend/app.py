4a4ee67f149b78e288c0105d851d9a7b8a7d0a85
James Riall <jamesrariall@gmail.com>
2019-09-01 08:31:51 +0100

    Set up SQLite database

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-Config.init()
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+db.app = app
 
 
 @app.route('/')
--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,14 @@ from dotenv import load_dotenv
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv()
+
 class Config:
   """Contains the configuration for the application."""
-
-  @classmethod
-  def init(cls):
-    load_dotenv()
-
-    cls.HOST = os.getenv('HOST', 'localhost')
-    cls.PORT = os.getenv('PORT', '5000')
-    cls.DEBUG = os.getenv('DEBUG', True)
+  HOST = os.getenv('HOST', 'localhost')
+  PORT = os.getenv('PORT', '5000')
+  DEBUG = os.getenv('DEBUG', True)
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
